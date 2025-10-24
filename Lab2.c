5b3f5df1b1bcb4153453e7884197ba1cdf784b53
Sean Gallaway <107509978+Sean-Gallaway@users.noreply.github.com>
2025-10-23 22:14:39 -0700

    Update Lab2.c

--- Lab2.c
+++ Lab2.c
@@ -8,13 +8,14 @@ int main () {
   pid_t pid = 0;
   int status;
   const int childNum = 10;
-  
+
+  // list of commands to use
   char *commands[][3] = {
     {"ls", "-l", NULL},
     {"echo", "Hello Sean", NULL},
     {"date", NULL, NULL},
     {"ps", NULL, NULL},
-    {"echo", "Hows your day?"},
+    {"echo", "Hows your day?", NULL},
     {"echo", "Message 2", NULL},
     {"echo", "Message 3", NULL},
     {"echo", "Message 4", NULL},
@@ -23,20 +24,26 @@ int main () {
   };
   
   printf("Parent: %d\n", getpid());
+  // spawn processes based on childNum
   for (int a = 0; a < childNum; a++) {
     pid = fork();
     if (pid < 0) {
+      // check for error
       perror("Failed to fork");
       exit(EXIT_FAILURE);
     }
     else if (pid == 0){
+      // if valid child, run one of the commands from the command list.
       printf("Process ID: %d\t Running: %s\n", getpid(), commands[a][0]);
       execvp(commands[a][0], commands[a]);
+
+      // check for error
       perror("exec failure on process %d", getpid());
       exit(EXIT_FAILURE);
     }
   }
-  
+
+  // wait for children to finish.
   while ((pid = wait(&status)) > 0) {
     printf("Process ID %d finished\n", pid);
   }
